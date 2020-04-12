06a588df1c840905a5517faf79c745380530bba3
mevolent <alexchobanov@gmail.com>
2020-04-12 18:45:49 +0300

    Created IIdentityContext as IAuthenticatinoContext implementation https://github.com/achobanov/web-application/issues/15

---
+++ src/Gateways/Web.Authentication/Web.Authentication/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Application.Contracts;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Blog.Gateways.Web.Authentication
+{
+    public class IdentityContext : IAuthenticationContext
+    {
+        private readonly HttpContext httpContext;
+        
+        public IdentityContext(IHttpContextAccessor httpContextAccessor)
+            => this.httpContext = httpContextAccessor.HttpContext;
+
+        public string UserId => this.httpContext.User.FindFirstValue(ClaimTypes.NameIdentifier); 
+    }
+}
