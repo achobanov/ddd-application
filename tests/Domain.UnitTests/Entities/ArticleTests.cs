02c10c2f82a891a57758106b7701f49faf090777
mevolent <alexchobanov@gmail.com>
2020-05-01 13:47:08 +0300

    Expanded Article tests https://github.com/achobanov/web-application/issues/21

--- tests/Domain.UnitTests/Entities/ArticleTests.cs
+++ tests/Domain.UnitTests/Entities/ArticleTests.cs
@@ -1,24 +1,37 @@
-﻿namespace Blog.Domain.UnitTests.Entities
-{
-    using Blog.Domain.Articles;
-    using Xunit;
+﻿using Blog.Domain.Articles;
+using Xunit;
 
+namespace Blog.Domain.UnitTests.Entities
+{
     public class ArticleTests
     {
-        [Fact]
-        public void TitleShouldThrowExceptionWhenNull()
-        {
-            // Assert
-            Assert.Throws<ArticleException>(
-                () => new Article(null, "Test Content"));
-        }
+        private const string SampleText = "Sample text";
 
         [Fact]
-        public void UserIdShouldThrowExceptionWhenNull()
-        {
-            // Assert
-            Assert.Throws<ArticleException>(
-                () => new Article("Test Title", "Test Content"));
-        }
+        public void Title_ShouldNotBeNull()
+            => Assert.Throws<ArticleException>(
+                () => new Article(null, SampleText));
+
+        [Fact]
+        public void Title_ShouldNotBeEmpty()
+            => Assert.Throws<ArticleException>(
+                () => new Article(string.Empty, SampleText));
+
+        [Theory]
+        [InlineData(41)]
+        [InlineData(100)]
+        public void Title_ShouldBeOfValidLength(int length)
+            => Assert.Throws<ArticleException>(
+                () => new Article(new string('x', length), SampleText));
+
+        [Fact]
+        public void Content_ShouldNotBeNull()
+            => Assert.Throws<ArticleException>(
+                () => new Article(SampleText, null));
+
+        [Fact]
+        public void Content_ShouldNotBeEmpty()
+            => Assert.Throws<ArticleException>(
+                () => new Article(SampleText, string.Empty));
     }
 }
