36be06567cbbda0e9d8b8d78a988a0274e08d4f0
mevolent <alexchobanov@gmail.com>
2020-05-02 16:17:27 +0300

    Improved variable naming https://github.com/achobanov/web-application/issues/21

--- Common/Mappings/MapCreatorInterfaces.cs
+++ Common/Mappings/MapCreatorInterfaces.cs
@@ -4,21 +4,21 @@ namespace Blog.Common.Mappings
 {
     public interface IMapFrom<T> : IMapCreator
     {
-        void IMapCreator.CreateMap(Profile mapper) => mapper.CreateMap(typeof(T), this.GetType());
+        void IMapCreator.CreateMap(Profile profile) => profile.CreateMap(typeof(T), this.GetType());
     }
 
     public interface IMapTo<T> : IMapCreator
     {
-        void IMapCreator.CreateMap(Profile mapper) => mapper.CreateMap(this.GetType(), typeof(T));
+        void IMapCreator.CreateMap(Profile profile) => profile.CreateMap(this.GetType(), typeof(T));
     }
 
     public interface IMapExplicitly
     {
-        void CreateMap(Profile mapper);
+        void CreateMap(Profile profile);
     }
 
     public interface IMapCreator
     {
-        void CreateMap(Profile mapper);
+        void CreateMap(Profile profile);
     }
 }
