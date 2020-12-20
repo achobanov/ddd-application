5d66f957bc37d91855f7d4dd07707af71ce66117
achobanov <alexchobanov@gmail.com>
2020-12-20 10:24:29 +0200

    Created Desktop.Core WPF project with Prism

---
+++ src/Gateways/Desktop/Desktop.Core/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using EnduranceContestManager.Gateways.Desktop.Core.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+
+namespace EnduranceContestManager.Gateways.Desktop.Core
+{
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override Window CreateShell()
+            => this.Container.Resolve<ShellWindow>();
+    }
+}
---
+++ src/Gateways/Desktop/Desktop.Core/Views/ShellWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace EnduranceContestManager.Gateways.Desktop.Core.Views
+{
+    public partial class ShellWindow : Window
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
