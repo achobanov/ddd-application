07c661f5fac384b41aec91c466d35008b73f48b3
achobanov <alexchobanov@gmail.com>
2020-12-20 12:04:07 +0200

    Added Desktop.Interfaces project

---
+++ src/Gateways/Desktop.Root/Desktop.Interfaces/IInitializerInterface.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EnduranceContestManager.Gateways.Desktop.Interfaces
+{
+    public interface IInitializerInterface
+    {
+        Task Run(IServiceProvider serviceProvider);
+    }
+}
