fdbf26c35e0a5b76ee8978d602e54dbbfaf61c75
achobanov <alexchobanov@gmail.com>
2021-01-31 16:34:46 +0200

    Removed Article files from Application

--- src/Application.root/Application.Interfaces/Blog/Articles/IArticleCommands.cs
+++
@@ -1,9 +0,0 @@
-using EnduranceContestManager.Application.Interfaces.Base;
-using EnduranceContestManager.Domain.Blog.Articles;
-
-namespace EnduranceContestManager.Application.Interfaces.Blog.Articles
-{
-    public interface IArticleCommands : ICommandRepository<Article>
-    {
-    }
-}
