fe0563da1d64c99e48515fc0fdd13e8ff01b8174
achobanov <alexchobanov@gmail.com>
2021-06-28 09:22:50 +0300

    Changed EnduranceEventView save button to "Save"

--- src/Gateways/Desktop/Core/ViewModels/ShardableFormBase.cs
+++ src/Gateways/Desktop/Core/ViewModels/ShardableFormBase.cs
@@ -8,7 +8,6 @@ using EnduranceJudge.Gateways.Desktop.Core.Components.Templates.ListItem;
 using EnduranceJudge.Gateways.Desktop.Services;
 using Prism.Commands;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
